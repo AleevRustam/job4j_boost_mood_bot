a7f56641687e13a4893b1f28bdbee428b845883f
AleevRustam <re1973@mail.ru>
2024-11-13 10:52:19 +0200

    Aware интерфейсы [#505301]

--- src/main/java/ru/job4j/servises/BotCommandHandler.java
+++ src/main/java/ru/job4j/servises/BotCommandHandler.java
@@ -3,10 +3,13 @@ package ru.job4j.servises;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.jvnet.hk2.annotations.Service;
+import org.springframework.beans.factory.BeanNameAware;
 import ru.job4j.content.Content;
 
 @Service
-public class BotCommandHandler {
+public class BotCommandHandler implements BeanNameAware {
+    private String beanName;
+
     void receive(Content content) {
         System.out.println(content);
     }
@@ -19,5 +22,11 @@ public class BotCommandHandler {
     @PreDestroy
     public void destroy() {
         System.out.println("Bean will be destroyed via @PreDestroy.");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+        System.out.println("Bean name from BeanNameAware: " + beanName);
     }
 }
--- src/main/java/ru/job4j/servises/TelegramBotService.java
+++ src/main/java/ru/job4j/servises/TelegramBotService.java
@@ -2,11 +2,14 @@ package ru.job4j.servises;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Service;
 import ru.job4j.content.Content;
 
 @Service
-public class TelegramBotService {
+public class TelegramBotService implements BeanNameAware {
+    private String beanName;
+
     private final BotCommandHandler handler;
 
     public TelegramBotService(BotCommandHandler handler) {
@@ -25,5 +28,11 @@ public class TelegramBotService {
     @PreDestroy
     public void destroy() {
         System.out.println("Bean will be destroyed via @PreDestroy.");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+        System.out.println("Bean name from BeanNameAware: " + beanName);
     }
 }
