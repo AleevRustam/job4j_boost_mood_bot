5653a32226a203293c16ae8bfa27bdf0399fa711
AleevRustam <re1973@mail.ru>
2024-11-16 10:18:08 +0200

    Spring Data JPA [#505298 #522107]

--- src/main/java/ru/job4j/repository/UserFakeRepository.java
+++ src/main/java/ru/job4j/repository/UserFakeRepository.java
@@ -1,6 +1,6 @@
 package ru.job4j.repository;
 
-import org.springframework.context.annotation.Profile;
+/*import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 import ru.job4j.model.User;
 import java.util.ArrayList;
@@ -26,4 +26,4 @@ public class UserFakeRepository implements UserRepository {
     public void save(User user) {
         userMap.put(user.getClientId(), user);
     }
-}
+}*/
